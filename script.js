ee33796d5c498c3b54446aa623076343409c534a
Sally Perez <seattlesally1@gmail.com>
2020-11-05 15:42:32 -0800

    added ability to save to local storage

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function init() {
     $("#currentDay").text(currentDate);
     // currentHour is a number 
     var currentHour = parseInt(currentTime.format("HH"));
+    console.log(`current hour is ${currentHour}`);
 
     // loop through hours and set past/current/future different colors
-    for (i = 9; i < 14; i++) {
+    for (i = 9; i <= 17; i++) {
         // make string of time two characters long to grab div
         var numString = i.toString();
         if( i < 10) {
@@ -25,21 +26,29 @@ function init() {
         } else {
             divEl.addClass("future");
         }
+
+        // display any saved diaries in local storage
+        // get local storage
+        var storedDiary = localStorage.getItem(numString) || [];
+        if (storedDiary != []) {
+            console.log(`data stored at ${numString} see ${storedDiary}`);
+        }
     }
 }
 
 // saveDiary - saves user input into local storage
 function saveDiary(event){
     // event.preventDefault; - do i need this?
-    // console.log("something was clicked!" + event.target);
-    var buttonEl = event.target;
+    var buttonEl = $(event.target);
     console.log(typeof buttonEl, buttonEl);
-    // var textEl = buttonEl.parentElement.find("textarea");
-    console.log(buttonEl.parentElement.child(".textarea"));
+    var textInput = buttonEl.siblings("textarea").val();
+
+    // save text to local storage
+    localStorage.setItem(buttonEl.attr("data-hour"), textInput);
 }
 
 
-// add listeners to buttons on page
+// add listeners to buttons on page to save any entries
 $(document).on("click", ".saveBtn", saveDiary);
 
 // call to initialize page
