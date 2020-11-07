c564cf14867498bb10ea1ecdf423502c1d348ca9
Sally Perez <seattlesally1@gmail.com>
2020-11-06 17:10:08 -0800

    added bootstrap grid classes for different size screens

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,17 @@
 var currentTime = moment();
 var currentDate = currentTime.format('dddd, MMMM Do');
 
+// init current page
 function init() {
     // show current date
     $("#currentDay").text(currentDate);
     // currentHour is a number 
     var currentHour = parseInt(currentTime.format("HH"));
-    console.log(`current hour is ${currentHour}`);
 
-    // loop through hours and set past/current/future different colors
+    // loop through hours 09 to 17 military time (9AM to 5PM)
+    // and set past/current/future different colors
     for (i = 9; i <= 17; i++) {
-        // make string of time two characters long to grab div
+        // make string of time two characters long to target each hour div
         var numString = i.toString();
         if( i < 10) {
             numString = `0${i}`;
@@ -27,33 +28,28 @@ function init() {
             divEl.addClass("future");
         }
 
-        // display any saved diaries in local storage
-        // get local storage
+        // display any saved diaries found in local storage
         var storedDiary = localStorage.getItem(numString) || "";
         if (storedDiary != "") {
             console.log(`data stored at ${numString} see ${storedDiary}`);
-            // put stored value in time slot diary
+            // display stored value in time div for that hour
             $(`#text-area-${numString}`).append(storedDiary);
-
-            // console.log(divEl);
         }
     }
 }
 
 // saveDiary - saves user input into local storage
-function saveDiary(event){
-    // event.preventDefault; - do i need this?
-    var buttonEl = $(event.target);
-    console.log(typeof buttonEl, buttonEl);
+function saveDiary(){
+    // get hour id from button clicked and any input from user in textarea    
+    var hour = $(this).attr("data-hour");
+    var textInput = $(this).siblings("textarea").val();
 
-    var textInput = buttonEl.siblings("textarea").val();
-
-    // need to add something if val is blank
-        // save text to local storage
-    if (textInput != "") {
-        localStorage.setItem(buttonEl.attr("data-hour"), textInput);
+    // if user cleared the text, remove entry from local storage
+    // or if user entered text, add to the local storage
+    if (!textInput) {
+        localStorage.removeItem(hour);
     } else {
-        localStorage.removeItem("data-hour");
+        localStorage.setItem(hour, textInput);
     }
 }
 
